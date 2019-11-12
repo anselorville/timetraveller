9ad51d6fd1a45cccc86e012692e947ee35723129
zerelazero <drinkdarkdoom@gmail.com>
2019-11-12 17:56:16 +0800

    初始化git项目

---
+++ src/main/java/com/zehoon/timetraveller/TimetravellerApplication.java
@@ -0,0 +1,13 @@
+package com.zehoon.timetraveller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetravellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimetravellerApplication.class, args);
+    }
+
+}
